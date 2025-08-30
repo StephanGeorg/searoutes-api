49476be6864a85bf77a9519ea3250c02da07405a
Stephan Georg <s.georg@hey.com>
2025-08-30 11:11:25 +0200

    Fix route tests

--- test/routes/searoutes.js
+++ test/routes/searoutes.js
@@ -5,14 +5,14 @@ import app from '../../src/server';
 
 describe('Endpoint /api/searoutes/shortest/', () => {
   describe('GET /api/searoutes/shortest/:startPoint/:endPoint', () => {
-    it('should return a list of users', (done) => {
+    it('should return sea route distance', (done) => {
       supertest(app)
         .get('/api/searoutes/shortest/13.5029,43.6214/20.2621,39.4982')
         .expect(200)
         .end((err, res) => {
           const { body } = res;
-          expect(body.distance).to.be.equal(746.2547255293554);
-          expect(body.distanceNM).to.be.equal(402.94546283265413);
+          expect(body.distance).to.be.equal(746.2536947598509);
+          expect(body.distanceNM).to.be.equal(402.9449062614448);
           done();
         });
     });
